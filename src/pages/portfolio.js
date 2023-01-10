f77e516b9b824b0e217bed44f5f3ce9b5969e575
Rahul T <rahulmanojcoder@gmail.com>
2023-01-10 23:44:24 +0530

    bug fix

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -15,7 +15,7 @@ const PortfolioPage = () => {
                 <meta property="og:site_name" content="Pravink Tatoos" />
                 <meta property="og:title" content="Pravink Tatoos" />
                 <meta property="og:url" content="https://pravinktattoos.com" />
-                <meta property="og:image" itemprop="image" content={ogImg} />
+                <meta property="og:image" itemprop="image" content="https://github.com/elaventa/pravinkTattoos/blob/d8f2b1f2217729d05d8732dba3ccf9708cfcc404/src/images/ogImage.png?raw=true" />
                 <meta property="og:type" content="website" />
                 <meta property="og:description" content="Pravink Tatoos" />
             </Helmet>
